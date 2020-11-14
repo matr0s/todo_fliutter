09bbaeeed4928217f3457b8f21bc5929643a4b9f
matr0s <serhiy.osadchuk@gmail.com>
2020-11-15 00:53:48 +0200

    Updated logic for the checkboxes - now it works

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,23 +1,55 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
   bool isChecked = false;
+
+  // void checkboxCallback(bool checkboxState) {
+  //   setState(() {
+  //     isChecked = checkboxState;
+  //   });
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return CheckboxListTile(
-      controlAffinity: ListTileControlAffinity.platform,
+    return ListTile(
       title: Text(
         'Buy milk',
         style: TextStyle(
           fontSize: 25.0,
           fontWeight: FontWeight.w500,
+          decoration:
+              isChecked ? TextDecoration.lineThrough : TextDecoration.none,
         ),
       ),
-      value: isChecked,
-      onChanged: (bool value) {
-        isChecked ? isChecked = !value : isChecked = value;
-        print(isChecked);
-      },
+      trailing: TaskCheckbox(
+        checkboxState: isChecked,
+        toggleCheckboxState: (bool checkboxState) {
+          setState(() {
+            isChecked = checkboxState;
+          });
+        },
+      ),
     );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  TaskCheckbox({this.checkboxState, this.toggleCheckboxState});
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: checkboxState,
+        onChanged: toggleCheckboxState);
+    // print('value: $value, isChecked $checkboxState');
   }
 }
